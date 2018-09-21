e238d98b65e3a75ef754fd0c6478bf7b34e1161d
adrantiev1 <35266709+adrantiev1@users.noreply.github.com>
2018-09-21 08:47:37 -0600

    on/off toggle button

--- simple-gallery/js/main.js
+++ simple-gallery/js/main.js
@@ -1,6 +1,31 @@
+// create a button in the gallery that toggles
+// the image on and off
 window.addEventListener('DOMContentLoaded', function(){
-    // All our code is global to the function
-    var gallery = document.querySelector('.gallery');
-    // Inline style on the element
-    gallery.style.backgroundColor = "blue";
+ var button = document.querySelector('button');
+
+button.addEventListener('click', onClickOnOff);
+
+function onClickOnOff(){
+
+    gallery.classList.toggle('hidden')
+}
+    
+//  button.classList.toggle();
+
+
+
 })
+
+// window.addEventListener('DOMContentLoaded', function(){
+//     // All our code is global to the function
+//     var gallery = document.querySelector('.gallery');
+//     // Inline style on the element
+//     // gallery.style.backgroundColor = "blue";
+
+//     // add remove a class from the attached css file
+//     // elment.classList.add()
+//     // elment.classList.remove()
+//     // elment.classList.toggle()
+//     gallery.classList.add('red');
+
+// })
