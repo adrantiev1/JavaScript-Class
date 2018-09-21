3da09f0bfbf90f7993c6632dd9d744a08186e2b4
adrantiev1 <35266709+adrantiev1@users.noreply.github.com>
2018-09-21 08:25:35 -0600

    Inline styling

---
+++ simple-gallery/js/main.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', function(){
+    // All our code is global to the function
+    var gallery = document.querySelector('.gallery');
+    // Inline style on the element
+    gallery.style.backgroundColor = "blue";
+})
