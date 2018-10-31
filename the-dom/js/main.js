1ee0fb064f333199d6b1d23b7171d6b8014cf528
adrantiev1 <35266709+adrantiev1@users.noreply.github.com>
2018-10-31 09:41:35 -0600

    injected object to a dome template

---
+++ the-dom/js/main.js
@@ -0,0 +1,49 @@
+"use-strict"
+window.addEventListener('load',function(){
+
+
+    const productTemplate = document.querySelector("template").content;
+    // getting data from the single.js file 
+    const productItemData = severalDataObjects;
+    console.log(severalDataObjects); 
+
+    // setDisplay()
+    manyItemsDisplay()
+
+    function manyItemsDisplay(){
+        productItemData.forEach(function(product,index){
+            let newItem = productTemplate.cloneNode(true);
+            // newItem.setAttribute("data-index",index);
+            const productTitle = newItem.querySelector('.card-title'); 
+            const productDesc = newItem.querySelector('.card-details'); 
+            const productUrl = newItem.querySelector('.card-thumbnail'); 
+        
+            productTitle.innerHTML =  product.title;
+            productUrl.src = product.url;
+            productDesc.innerHTML = product.descrption;
+
+            console.log(newItem);  
+            document.body.appendChild(newItem);  
+            
+        })
+    }
+
+    // set up single template Item
+    // function setDisplay(){
+    //     const productTitle = productTemplate.querySelector('.card-title'); 
+    //     const productDesc = productTemplate.querySelector('.card-details'); 
+    //     const productUrl = productTemplate.querySelector('.card-thumbnail'); 
+
+       
+        
+    //     productTitle.innerHTML =  productItemData.map(x=>x.title);
+    //     productUrl.src = productItemData.map(x=>x.url);
+    //     productDesc.innerHTML = productItemData.map(x=>x.descrption);
+        
+
+    //     console.log(productTemplate);  
+    //     document.body.appendChild(productTemplate);      
+        
+    // }
+    
+});
